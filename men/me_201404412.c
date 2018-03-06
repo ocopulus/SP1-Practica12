0df53cffc3f0b834ac56aa032769756a5796a862
ocopulus <jlemus003@gmail.com>
2018-03-06 14:32:37 -0600

    cambios en men

--- men/me_201404412.c
+++ men/me_201404412.c
@@ -36,9 +36,9 @@ si_meminfo(&i);
 for (lru = LRU_BASE; lru < NR_LRU_LISTS; lru++)
 pages[lru] = global_page_state(NR_LRU_BASE + lru);
 seq_printf(m,"{\"total\": %8lu, \"libre\": %8lu, \"utilizado\": %lu}\n",
-		K(i.totalram),
-		K(i.freeram),
-		( K(i.totalram) - K(i.freeram) ) * 100 / K(i.totalram));
+		i.totalram*4,
+		i.freeram*4,
+		((i.freeram*100)/i.totalram));
 #undef K
 return 0;
 
